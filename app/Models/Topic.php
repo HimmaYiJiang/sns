aaedd72ca1c98aef842327b23a98f3624ba7c068
HimmaYiJijiang <27532431@qq.com>
2017-12-07 14:29:35 +0800

    User can create reply

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,9 +5,15 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
     }
 
     public function user()
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
+
         //
         \Carbon\Carbon::setLocale('zh');
         \App\Models\Topic::observe(TopicObserver::class);
