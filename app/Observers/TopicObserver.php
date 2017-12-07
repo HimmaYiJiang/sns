794ff670ebc5effc4f8e05dc3f8fb64f23f620fc
HimmaYiJijiang <27532431@qq.com>
2017-12-07 16:06:45 +0800

    Integrate laravel-permission package

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -20,7 +20,7 @@ class TopicObserver
         $topic->excerpt = make_excerpt($topic->body);
 
     }
-    
+
     public function deleted(Topic $topic)
     {
         \DB::table('replies')->where('topic_id', $topic->id)->delete();
